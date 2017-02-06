4d21f4dbf127589d601c18afdc78dcc396ad176d
Collins Udanor <collins.udanor@gmail.com>
2017-02-06 17:22:40 +0100

    Update check.php

--- tissue/check.php
+++ tissue/check.php
@@ -1,13 +1,21 @@
 <?php
-$host="localhost"; // Host name 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tissue');
+$connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+
+/*$host="localhost"; // Host name 
 $username="root"; // Mysql username 
 $password=""; // Mysql password 
 $db_name="tissue"; // Database name 
+*/
 $tbl_name="upload"; // Table name 
 
 // Connect to server and select databse.
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
+//mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+//mysql_select_db("$db_name")or die("cannot select DB");
 
 ?>
 
