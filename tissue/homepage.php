312407914a569e88b435f9645518e5eef45367d7
Collins Udanor <collins.udanor@gmail.com>
2017-02-06 17:24:50 +0100

    Update homepage.php

--- tissue/homepage.php
+++ tissue/homepage.php
@@ -3,15 +3,23 @@ session_start();
 ?>
 <?php
 $userid = $_SESSION['userid'];
-$host="localhost"; // Host name 
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'tissue');
+$connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+/*$host="localhost"; // Host name 
 $username="root"; // Mysql username 
 $password=""; // Mysql password 
 $db_name="tissue"; // Database name 
+*/
 $tbl_name="user"; // Table name
 
 
-mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
-mysql_select_db("$db_name")or die("cannot select DB");
+//mysql_connect("$host", "$username", "$password")or die("cannot connect"); 
+//mysql_select_db("$db_name")or die("cannot select DB");
 $sql="SELECT names FROM $tbl_name WHERE userid='$userid'";
 $result=mysql_query($sql);
 $row = mysql_fetch_assoc($result);
